6819a2ba28d2849797507b1eedad8af964d731c3
Abhishek Pandey <abhishekpandey8520@gmail.com>
2024-07-08 23:35:41 +0530

    port updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const hisaabRouter = require('./routes/hisaabRouter');
 const authMiddleWares = require('./middlewares/authMiddlewares')
 const { hisaabModel } = require('./models/hisaab-models')
 const { userModel } = require('./models/user-models')
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 // Serve static files from the 'public' directory
